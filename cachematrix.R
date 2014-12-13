4cb2a62c563c93e9eef8c02ac14dde4a9b10b70d
fcolmenero <fcolmenero78@gmail.com>
2014-12-13 13:29:02 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -9,25 +9,26 @@
 ## is storaged in internal function environment and get()/set() properties operations are created as inner functions
 ## (it's like a method emulation). A matrix and its inverse form can be storaged to be retrieve from cacheSolve function later
 makeCacheMatrix <- function(x = matrix()) {
-	  ##Cache storage for inverse matrix result
-	  m <- NULL
+	
+	##Cache storage for inverse matrix result
+	m <- NULL
 
-	  ## Set operation to load a new matrix in cache. Previous inverse result is cleared
+	## Set operation to load a new matrix in cache. Previous inverse result is cleared
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
 
-	  ## Get operation to return the real matrix
+	## Get operation to return the real matrix
         get <- function() x
 		
-	  ## Set operation to load inverse matrix result in cache
+	## Set operation to load inverse matrix result in cache
         setinverse <- function(inverse) m <<- inverse
 	
-	  ## Get operation to return the inverse matrix result from cache
+	## Get operation to return the inverse matrix result from cache
         getinverse <- function() m
 
-	  ## Matrix cache operation list
+	## Matrix cache operation list
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -44,12 +45,13 @@ cacheSolve <- function(x, ...) {
 		## Hit cache case. Returning previous result with no extra calculations	
               message("getting cached data")
               return(m)
-      }
+      	}
+      	
 	## No result was found in matrix cache. Solving matrix and saving result in cache...
-      data <- x$get()
-      m <- solve(data, ...)
-      x$setinverse(m)
+      	data <- x$get()
+      	m <- solve(data, ...)
+      	x$setinverse(m)
 
 	##Returning the computed result
-      m
+      	m
 }
